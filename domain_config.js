22a34a8ad9bcdbef044caf799aea19f34efb4a3c
robot <robot@superVideo.com>
2022-09-28 01:10:38 +0800

    新域名

--- domain_config.js
+++ domain_config.js
@@ -1,2 +1,2 @@
-export const baseUrl = "http://im.72sd.xyz";
-export const ws_url = "ws://ws.72sd.xyz/chat/socket";
+export const baseUrl = "http://ws.t6prn1.xyz";
+export const ws_url = "ws://ws.t6prn1.xyz/chat/socket";
