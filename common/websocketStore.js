b16ef51d80c46e3a9ca5da2b462eb749a209686d
simon <jyj5241@icloud.com>
2022-11-13 23:25:49 +0800

    1:pick websocket sessionId

--- common/websocketStore.js
+++ common/websocketStore.js
@@ -28,8 +28,10 @@ export default new Vuex.Store({
 				state.socketTask.close();
 			}
 			var i = Math.floor((Math.random() * store.state.req_url.length));
+			let websocket_id = uni.getStorageSync("websocket_id");
+			console.log("hahahahahhaahhahahah2",websocket_id)
 			state.socketTask = uni.connectSocket({
-				url: store.state.socket_url[i]+"/chat/socket"+"?app_uuid="+store.state.app_uuid,
+				url:store.state.socket_url[i]+"/chat/socket"+"?app_uuid="+store.state.app_uuid+"&session_id"+websocket_id,
 				// 【非常重要】必须确保你的服务器是成功的,如果是手机测试千万别使用ws://127.0.0.1:9099【特别容易犯的错误】
 				success(data) {
 					state.lock = false;	
