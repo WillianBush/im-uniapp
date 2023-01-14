1640db561dfc7a53acde3bb18466d675511231b5
rugal1837 <rugal1837@gmail.com>
2023-01-14 21:12:45 +0800

    修复iframe to web-view

--- pages/faxian/webview.vue
+++ pages/faxian/webview.vue
@@ -1,9 +1,7 @@
 <template>
 	<view>
-		<div class="container">
-			<cu-custom class="header" bgColor="bg-blue" :isBack="true"><block slot="content">VIP</block></cu-custom>
-			<iframe :src="url" style="width: 101%;border: none;height:93.5%"   />
-		</div>
+		<cu-custom bgColor="bg-blue" :isBack="true"><block slot="content">VIP</block></cu-custom>
+		<web-view :src="url" style="height:93.2vh;top:50px"></web-view>
 	</view>
 </template>
 
@@ -11,25 +9,12 @@
 	export default {
 		data() {
 			return {
-				url: '',
-				setHeight:0,
+				url: ''
 			}
 		},
 		onLoad(e) {
 			// 获取传递过来的链接
 			this.url = e.url
-
 		}
 	}
 </script>
-<style>
-	.container{
-		overflow:hidden;
-		position: absolute;
-		right: 0px;
-		left: 0;
-		top: 0px;
-		bottom: 0px;
-	}
-	.header{}
-</style>
