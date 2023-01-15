093c9bea7f93c812fdd42d5f68a75833a6f77f05
Universeside <122682700+Universeside@users.noreply.github.com>
2023-01-15 18:14:32 +0800

    添加翻译

--- pages/chat/user/note.vue
+++ pages/chat/user/note.vue
@@ -1,7 +1,7 @@
 <template>
 	<view>
-		<cu-custom bgColor="bg-blue"  :isBack="true" :nameToLeft="true"><block slot="backText"></block><block slot="content">备注</block><block slot="right">
-			<uni-text @tap="tijiao()" style="font-size: 22px;color: #fff;margin-right: 14px;font-size: 30upx;" class="lg text-gray ">提交</uni-text>
+		<cu-custom bgColor="bg-blue"  :isBack="true" :nameToLeft="true"><block slot="backText"></block><block slot="content">{{i18n.remark}}</block><block slot="right">
+			<uni-text @tap="tijiao()" style="font-size: 22px;color: #fff;margin-right: 14px;font-size: 30upx;" class="lg text-gray ">{{i18n.submit}}</uni-text>
 		</block></cu-custom>
 
 		<view class="cu-form-group margin-top" style="
@@ -9,7 +9,7 @@
 		margin-top: 15px;">
 				<input  maxlength="-1" v-model="txt" placeholder="请输入备注"/>
 			</view>
-			<view style="margin-top:20upx;color:#999;margin-left:30upx;font-size:26upx">原昵称：{{$store.state.cur_chat_entity.nickName}}</view>
+			<view style="margin-top:20upx;color:#999;margin-left:30upx;font-size:26upx">{{i18n.originalnickname}}：{{$store.state.cur_chat_entity.nickName}}</view>
 
 
 
